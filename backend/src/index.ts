43a3b2832871276ebd51f84eb7a494c871626423
lukas <lukas.noreikis@gmail.com>
2025-03-23 14:17:54 +0200

    basic backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,32 @@
+import http from 'http'
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
 
-const test: string = "hello world"
 
-console.log(test)
+if (!process.env.NODE_ENV) {
+  throw new Error("NODE_ENV is undefined")
+}
+
+const PORT = process.env.PORT || 8000
+
+// Initialize express
+const app = express()
+app.disable('x-powered-by')
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  methods: "GET"
+}))
+
+// Start server
+const server = http.createServer(app)
+server.listen(PORT, () => {
+  console.log(
+    `[${(process.env.NODE_ENV!)}]\nServer started on PORT ${PORT}`
+  )
+})
+
+// Start MongoDB
+mongoose.connect(process.env.MONGODB_URI as string)
+  .then(() => { console.log("Connected to the Database") })
+  .catch((error) => { console.log(error) });
