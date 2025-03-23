c181c8e7670e6baa0786eac02aeb198045727675
lukas <lukas.noreikis@gmail.com>
2025-03-23 14:23:01 +0200

    add theme provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Theme } from '@radix-ui/themes'
 import App from './App.tsx'
+import './index.css'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Theme appearance='light'>
+      <div className='app-container'>
+        <App />
+      </div>
+    </Theme>
   </StrictMode>,
 )
